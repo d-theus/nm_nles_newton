5791364d6b3b54afcb1a118826affb7224babf70
slowness pc <slma0x02@gmail.com>
2013-05-07 19:07:45 +0000

    classes instead of vectors

--- main.cpp
+++ main.cpp
@@ -8,6 +8,11 @@ using std::vector;
 using std::cout;
 using std::endl;
 
+class MF2;
+class F2;
+class V2;
+F2 grad(function<double(V2)>);
+
 class V2 {
 public:
 	double x,y;
@@ -20,51 +25,67 @@ public:
 const V2 hx(10e-10,0);
 const V2 hy(0,10e-10);
 
-typedef vector<function<double(V2)> > F2;
-typedef vector<vector<function<double(V2)> > > MF2;
+class MF2 {
+public:
+	function<double(V2)> f11,f12,f21,f22;
+	MF2(){}
+	MF2 (function<double(V2)> f1,function<double(V2)> f2,
+	    function<double(V2)> f3,function<double(V2)> f4){
+		this->f11 = f1;
+		this->f12 = f2;
+		this->f21 = f3;
+		this->f22 = f4;
+	}
+};
 
+class F2 {
+public:
+	function<double(V2)> f1,f2;
+	F2(){}
+	F2 (function<double(V2)> f1,function<double(V2)> f2){
+		this->f1 = f1;
+		this->f2 = f2;
+	}
+	MF2 jacobian() const {
+		MF2 res(
+				grad(f1).f1,
+				grad(f1).f2,
+				grad(f2).f1,
+				grad(f2).f2
+		       );
+		return res;
+	}
+};
 F2 grad(function<double(V2)> f){
-	F2 res(2);
-	res[0] = [f](V2 x){
+	F2 res(
+	[f](V2 x){
 		return (f(x+hx) - f(x))/hx.norm();
-	};
-	res[1] = [f](V2 x){
+	},
+	[f](V2 x){
 		return (f(x+hy) - f(x))/hy.norm();
-	};
+	});
 	return res;
-}
-
-MF2 Jacob(function<double(V2)> f1,function<double(V2)> f2){
-	MF2 res(2);
-	res[0] = grad(f1);
-	res[1] = grad(f2);
-	return res;
-}
+};
 
 double diff(V2 p1, V2 p2) {return fabs(p1.x-p2.x) > fabs(p1.y-p2.y)?fabs(p1.x-p2.x):fabs(p1.y-p2.y);}
 double det(const MF2 &m,const V2 &p){
-	return m[0][0](p) * m[1][1](p) - m[1][0](p) * m[0][1](p);
+	return m.f11(p) * m.f22(p) - m.f12(p) * m.f21(p);
 }
 
 V2 find_root(F2 f, V2 p0, double eps){
 	V2 p1 = p0;
 	V2 dp(0,0);
-	MF2 J = Jacob(f[0],f[1]);
-	MF2 A1(2), A2(2);
+	MF2 J = f.jacobian();
+	MF2 A1, A2;
 
-	A1[0] = F2(2);
-	A1[1] = F2(2);
-	A2[0] = F2(2);
-	A2[1] = F2(2);
-
-	A1[0][0] = f[0];
-	A1[0][1] = J[0][1];
-	A2[0][0] = J[0][0];
-	A2[0][1] = f[0];
-	A1[1][0] = f[1];
-	A1[1][1] = J[1][1];
-	A2[1][0] = J[1][0];
-	A2[1][1] = f[1];
+	A1.f11 = f.f1;
+	A1.f12 = J.f12;
+	A2.f11 = J.f11;
+	A2.f12 = f.f1;
+	A1.f21 = f.f2;
+	A1.f22 = J.f22;
+	A2.f21 = J.f21;
+	A2.f22 = f.f1;
 	do{
 		p0 = p1;
 		p1.x = p0.x - det(A1,p0) / det (J, p0);
@@ -75,9 +96,9 @@ V2 find_root(F2 f, V2 p0, double eps){
 
 int main(int argc, const char *argv[])
 {
-	F2 f(2);
-	f[0] = [](V2 p) {return p.x*p.x + p.y*p.y -1 ;};
-	f[1] = [](V2 p) {return p.y - p.x - 0.5 ;};
+	F2 f;
+	f.f1 = [](V2 p) {return p.x*p.x + p.y*p.y -1 ;};
+	f.f2 = [](V2 p) {return p.y - p.x - 0.5 ;};
 	V2 result = find_root(f,V2(1,1), 0.00001);
 	cout << result.x << ", " << result.y << endl;
 	return 0;
